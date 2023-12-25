cb1a8a6cdd1a7780007fef72e0c2bc892f74fd54
syncd010 <antonio.mendes.silva@gmail.com>
2023-12-25 20:33:40 +0000

    Improve day 25

--- src/day25.cpp
+++ src/day25.cpp
@@ -18,68 +18,79 @@ namespace aoc25 {
 using namespace std;
 using namespace aoc;
 
-using Graph = unordered_map<string, vector<string>>;
+using Graph = unordered_map<int, vector<int>>;
 
+// Parses input into a graph. Changes vertices names to ints for performance
 Graph parseInput(const string &input) {
   Graph graph;
   auto lines = toVector(input | splitString('\n'));
+  unordered_map<string_view, int> mapIds{};
+
+  int nextId = 0;
+  auto lookupOrInsert = [&mapIds, &nextId](string_view s) {
+    if (!mapIds.contains(s)) mapIds[s] = nextId++;
+    return mapIds[s];
+  };
 
   for (auto line: lines) {
     auto p = line.find(':');
-    auto edges = toVector(line.substr(p + 2) | splitString<string>(' '));
-    auto v1 = string(line.substr(0, p));
-    if (!graph.contains(v1)) graph[v1] = vector<string>{};
-    for (auto v2 : edges) {
+    auto edges = toVector(line.substr(p + 2) | splitString(' '));
+    auto v1 = lookupOrInsert(line.substr(0, p));
+    if (!graph.contains(v1)) graph[v1] = {};
+    for (auto v : edges) {
+      auto v2 = lookupOrInsert(v);
       graph[v1].push_back(v2);
-      if (!graph.contains(v2)) graph[v2] = vector<string>{};
+      if (!graph.contains(v2)) graph[v2] = {};
       graph[v2].push_back(v1);
     }
   }
   return graph;
 }
 
-void contract(Graph &graph, const string &v1, const string &v2, const string &vNew) {
+// Contracts edge v1-v2 creating the new vertice newV
+void contract(Graph &graph, const int v1, const int v2, const int newV) {
   for (auto v : graph[v1]) {
     if (v == v2) continue;
     graph[v].erase(find(graph[v].begin(), graph[v].end(), v1));
-    graph[v].push_back(vNew);
-    graph[vNew].push_back(v);
+    graph[v].push_back(newV);
+    graph[newV].push_back(v);
   }
 
   for (auto v : graph[v2]) {
     if (v == v1) continue;
     graph[v].erase(find(graph[v].begin(), graph[v].end(), v2));
-    graph[v].push_back(vNew);
-    graph[vNew].push_back(v);
+    graph[v].push_back(newV);
+    graph[newV].push_back(v);
   }
 }
 
 Result solvePartOne(const string &input) {
   auto originalGraph = parseInput(input);
-
   default_random_engine generator(time(0));
   uniform_int_distribution<int> dist(0, originalGraph.size() * 10);
 
+  const int sz = originalGraph.size();
   bool partitioned = false;
   int64_t res = 0;
   while (!partitioned) {
     auto graph{originalGraph};
     auto rgKeys = graph | views::keys;
+    // Set with remaining vertices, not contracted
     auto vertices = unordered_set(rgKeys.begin(), rgKeys.end());
-    auto rgCounts = rgKeys | views::transform([](auto k) { return make_pair(k, 1);});
-    unordered_map<string, int> vertCounts(rgCounts.begin(), rgCounts.end());
+    // Contracted vertices counts to get final result
+    auto vertCounts = vector(sz * 2 - 2, 1);
 
-    while(vertices.size() > 2) {
+    // Contract graph until 2 vertices remain    
+    for(int nextV = sz; vertices.size() > 2; nextV++) {
       auto v1 = vertices.extract(next(vertices.begin(), (dist(generator) % vertices.size()))).value();
       auto v2 = graph[v1][(dist(generator) % graph[v1].size())];
       vertices.erase(v2);
-
-      auto vNew = to_string(graph.size());
-      graph[vNew] = vector<string>{};
-      contract(graph, v1, v2, vNew);
-      vertices.insert(vNew);
-      vertCounts[vNew] = vertCounts[v1] + vertCounts[v2];
+      vertices.insert(nextV);
+      graph[nextV] = {};
+      contract(graph, v1, v2, nextV);
+      vertCounts[nextV] = vertCounts[v1] + vertCounts[v2];
     }
+    // Check if we got the min cut
     partitioned = (graph[*vertices.begin()].size() <= 3);
     if (partitioned) {
       res = accumulate(vertices.begin(), vertices.end(), 1, [&vertCounts](auto acc, auto v) { return acc * vertCounts[v];});
